589751b634afd7dece70a1b1d944f7ae70ccfbda
MiestnyLovecZien <48026495+MiestnyLovecZien@users.noreply.github.com>
2019-03-15 15:35:30 +0000

    15/3/19

--- WordSearchPuzzle.java
+++ WordSearchPuzzle.java
@@ -4,26 +4,11 @@ import java.util.Collections;
 public class WordSearchPuzzle {
     private char[][] puzzle ;
     private ArrayList<String> puzzleWords ;
-
+    
     public WordSearchPuzzle(ArrayList<String> userSpecifiedWords){
-	userSpecifiedWords = puzzleWords;
-        // puzzle generation using user specified words
-        // The user passes in a list of words to be used
-        // for generating the word search grid.
     }
     public WordSearchPuzzle(String wordFile, int wordCount, int shortest, int longest){
-	// puzzle generation using words from a file
-        // The user supplies the filename. In the file 
-        // the words should appear one per line.
-        // The wordCount specifies the number of words
-        // to (randomly) select from the file for use in
-        // the puzzle.
-        // shortest and longest specify the shortest
-        // word length to be used and longest specifies
-        // the longest word length to be used.
-        // SO, using the words in the file randomly select
-        // wordCount words with lengths between shortest
-        // and longest.
+    
     }
 
     // The dimensions of the puzzle grid should be set
@@ -38,13 +23,20 @@ public class WordSearchPuzzle {
     // of the character total.
     //
     // You will also need to add the methods specified below
-    public ArrayList<String> getWordSearchList(){
-        return puzzleWords;
-    }
-    /*public char[][] getPuzzleAsGrid(){
+    /*public ArrayList<String> getWordSearchList(){
         
+    }*/
+    public char[][] getPuzzleAsGrid(){
+        int puzzleSize = puzzleWords.size();
+        String allWords = "";
+        for(int i = 0; i < puzzleSize; i++){
+            allWords += puzzleWords.get(i);           
+            }
+           int puzzleLength = (int)(allWords.length()*1.75);
+        puzzle = new char[puzzleLength][puzzleLength];
+        return puzzle;
     }
-    public String getPuzzleAsString(){
+    /*public String getPuzzleAsString(){
         
     }
     public void showWordSearchPuzzle(boolean hide){
