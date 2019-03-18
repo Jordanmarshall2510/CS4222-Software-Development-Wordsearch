a9a583030ea285eec97605f69b54c6bfea0487cc
Jordan Marshall <47557144+Jordanmarshall2510@users.noreply.github.com>
2019-03-18 01:23:21 +0000

    Created a driver program

---
+++ WordSearchPuzzleDriver.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+public class WordSearchPuzzleDriver{
+	
+	public static void main(String args[]){
+		
+		ArrayList<String> example = new ArrayList<String>();
+		example.add("Yellow");
+		example.add("Red");
+		example.add("Blue");
+		example.add("Green");
+
+		//Puzzle generation using user specified words
+
+		
+
+	}
+}
