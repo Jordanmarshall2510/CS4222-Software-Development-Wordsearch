249f3175d4af49ec748baa897a59265a953c0fbd
Jordan Marshall <jordan.marshall2510@gmail.com>
2019-04-10 20:45:57 +0100

    Update WordSearchPuzzleDriver.java

--- WordSearchPuzzleDriver.java
+++ WordSearchPuzzleDriver.java
@@ -1,28 +1,20 @@
-public class WordSearchPuzzleDriver{
-	public static void main(String args[]){
-		WorkSearchPuzzle test = new WorkSearchPuzzle();
-
-		private ArrayList<String> puzzleWords;
-
-		puzzleWords.addword("Hello");
-		puzzleWords.addword("Test");
-
-		puzzleWords.display();
-
 import java.util.ArrayList;
 
 public class WordSearchPuzzleDriver{
 	
 	public static void main(String args[]){
-		
 		ArrayList<String> example = new ArrayList<String>();
-		example.add("Yellow");
-		example.add("Red");
-		example.add("Blue");
-		example.add("Green");
+				example.add("Yellow");
+				example.add("Red");
+				example.add("Blue");
+				example.add("Green");
 
-		//Puzzle generation using user specified words
+		//Generates a wordsearch puzzle using an arrayList
+		WordSearchPuzzle puzzleArrayList = new WordSearchPuzzle(example); //Creates a new instance of WordSearch Puzzle using an ArrayList
+		generateWordSearchPuzzle();
 
-
+		//Generates a wordsearch puzzle using a file
+		WordSearchPuzzle puzzleFile = new WordSearchPuzzle("wordFile", 6, 3, 10);
+		generateWordSearchPuzzle();
 	}
 }
