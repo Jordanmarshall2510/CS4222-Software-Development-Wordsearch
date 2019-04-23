e0fcda5e69991ff694c933e6e9d8e96929eb92a6
Qwertywashere <47777528+Qwertywashere@users.noreply.github.com>
2019-04-23 11:40:17 +0100

    Update WordSearchPuzzleDriver.java

--- WordSearchPuzzleDriver.java
+++ WordSearchPuzzleDriver.java
@@ -9,21 +9,21 @@ public class WordSearchPuzzleDriver{
         //Shortest length is 5 and longest length is 5.
         WordSearchPuzzle test1 = new WordSearchPuzzle("wordFile500.txt",10,5,5);
         test1.getWordSearchList() ;
-        test1.showWordSearchPuzzle() ;
+        test1.showWordSearchPuzzle(false) ;
         System.out.println();
 
         //File wordFile500 with 7 words.
         //Shortest length is 3 and longest length is 5.
         WordSearchPuzzle test2 = new WordSearchPuzzle("wordFile500.txt",7,3,5);
         test2.getWordSearchList() ;
-        test2.showWordSearchPuzzle() ;
+        test2.showWordSearchPuzzle(false) ;
         System.out.println();
 
         //File wordFile500 with 8 words.
         //Shortest length is 4 and longest length is 10.
         WordSearchPuzzle test3 = new WordSearchPuzzle("wordFile500.txt",8,4,10);
         test3.getWordSearchList() ;
-        test3.showWordSearchPuzzle() ;
+        test3.showWordSearchPuzzle(false) ;
         System.out.println();
 
     //ArrayList Testing
@@ -38,7 +38,7 @@ public class WordSearchPuzzleDriver{
         //
         WordSearchPuzzle testList = new WordSearchPuzzle(example);
         testList.getWordSearchList() ;
-        testList.showWordSearchPuzzle() ;
+        testList.showWordSearchPuzzle(false) ;
         System.out.println();
 
     }
